ceae00704af09bb5e0d1b0dfa4e6c65c61f6d71d
David Anderson <davea@ssl.berkeley.edu>
2015-12-06 22:50:48 -0800

    prediction is working

--- football.py
+++ football.py
@@ -1,3 +1,6 @@
+from football2 import *
+import pickle
+
 def r():
     w = []
     f = open('ncaa_football.txt', 'r')
@@ -7,8 +10,7 @@ def r():
             w.append(line)
     return w
 
-def teams():
-    t = []
+def get_teams():
     f = r()
     for l in f:
         x = True
@@ -19,8 +21,7 @@ def teams():
             
         if x == False:
             l = l.split(' (')
-            t.append(l[0])
-    return t
+            teams.append(l[0])
 
 #ts = teams()            
 #for t in ts:
@@ -33,29 +34,75 @@ def int_check(c):
         return False
     return True
 
-
-def games():
-    t = teams()
-    g = []
+# parse the data file, create teams and games global variables
+#
+def get_games():
+    get_teams()
     f = r()
     for line in f:
         words = line.split('\t')
         if len(words) >= 7:
+            #print(line)
             t2 = words[3].replace('*', '')
-            if t1 < t2:
-                d = [t1, t2, words[5], words[6]]
-                g.append(d)
+            if t1 > t2:
+                continue
+            if  t2 not in teams:
+                continue
+            if not int_check(words[5]):
+                continue
+            if not int_check(words[6]):
+                continue
+            d = [
+                teams.index(t1), teams.index(t2),
+                int(words[5]), int(words[6])
+                ]
+            games.append(d)
         else:
              words = line.split(' (')
              t1 = words[0]
-    return g
 
-g = games()
-print(g)
-print(len(g))
-                
-                
+def create_info_file():
+    get_games()
+    ratings = predict()
+    x = [teams, games, ratings]
+    f = open('data.pickle', 'wb')
+    pickle.dump(x, f)
+    f.close()
+    print('done')
 
+def read_info_file():
+    global teams, games
+    f = open('data.pickle', 'rb')
+    x = pickle.load(f);
+    f.close();
+    teams = x[0]
+    games = x[1]
+    return x[2]
 
+def rankings():
+    count = 0
+    ratings = read_info_file()
+    pairs = {}
+    totals = []
+    for team in teams:
+        r = ratings[count * 2] / ratings[count*2+1]
+        pairs[r] = team
+        totals.append(r)
+        count += 1
+    totals.sort()
+    count = 0
+    for t in totals:
+        print(128-count, t, pairs[t])
+        count += 1
+        
+
+def test():
+    get_games()
+    print(games)
+    print(len(games))
+    ratings = predict()
+    predict_score(9, 6, ratings)
+
+rankings()
  
     
--- football2.py
+++ football2.py
@@ -1,45 +1,28 @@
 import numpy as np
 from scipy.optimize import minimize
 
-class Team:
-    def __init__(self, name):
-        self.name = name
-    def __repr__(self):
-        return 'name: %s' % (self.name)
+teams = []
+games = []
 
-class Game:
-    def __init__(self, team1, team2, score1, score2):
-        self.team1 = team1  # index of team in teams list
-        self.team2 = team2
-        self.score1 = score1
-        self.score2 = score2
-
-t1 = Team('Berkeley Bumblebees')
-t2 = Team('Albany Anteaters')
-t3 = Team('Emeryville Escargot')
-teams = [t1, t2, t3]
-
-g1 = Game(0, 1, 21, 14)
-g2 = Game(1, 2, 28, 7)
-games = [g1, g2]
-
-print(teams)
+# a game is described by a list
+# team1, team2, score1, score2
 
 # x is list of team offense/defense estimates
 # x[0]/x[1]: off/def for team 0
 # x[2]/x[3]: off/def for team 1
 # ...
 def score_error(x):
+    global games
     sum = 0
     for g in games:
-        o1 = x[g.team1*2]
-        d1 = x[g.team1*2+1]
-        o2 = x[g.team2*2]
-        d2 = x[g.team2*2+1]
+        o1 = x[g[0]*2]
+        d1 = x[g[0]*2+1]
+        o2 = x[g[1]*2]
+        d2 = x[g[1]*2+1]
         p1 = o1*d2
         p2 = o2*d1
-        e1 = p1 - g.score1
-        e2 = p2 - g.score2
+        e1 = p1 - g[2]
+        e2 = p2 - g[3]
         sum += e1**2
         sum += e2**2
         sum += (d1-1)**2
@@ -47,6 +30,7 @@ def score_error(x):
     return sum
 
 def predict_score(i, j, x):
+    global teams
     o1 = x[i*2]
     d1 = x[i*2+1]
     o2 = x[j*2]
@@ -55,14 +39,31 @@ def predict_score(i, j, x):
     p2 = o2*d1
     team1 = teams[i]
     team2 = teams[j]
-    print(team1.name, p1, team2.name, p2)
+    print(team1, p1, team2, p2)
 
-def foo2():
-    x0 = np.array([10, 1, 10, 1, 10, 1])
+def predict():
+    global teams, games
+    ratings = []
+    for t in teams:
+        ratings.append(20)
+        ratings.append(1)
+    x0 = np.array(ratings)
     res = minimize(score_error, x0, method='powell', options={'xtol': 1e-8, 'disp': True})
-    print(res.x)
+    return res.x
+
+def test():
+    global teams, games
+    teams = ['Berkeley Bumblebees', 'Albany Anteaters', 'Emeryville Escargot']
+
+    g1 = Game(0, 1, 21, 14)
+    g2 = Game(1, 2, 28, 7)
+    games = [g1, g2]
+
+    x = predict()
+    print(teams)
+    print(x)
     for i in range(3):
         for j in range(i+1, 3):
-            predict_score(i, j, res.x)
+            predict_score(i, j, x)
 
-foo2()
+#test()
