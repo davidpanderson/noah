8bb91f80ec435fc24827a11220acf6d88f56a1fe
David Anderson <davea@ssl.berkeley.edu>
2018-04-15 23:08:36 -0700

    boggle tweaks

--- boggle_dpa.py
+++ boggle_dpa.py
@@ -1,9 +1,8 @@
-import math
+import math, sys
 from dict import *
 
-
-
-# returns True if the num1 and num2 positions in the grid are adjacent
+# return True if the num1 and num2 positions in the grid are adjacent
+#
 def adjacent(num1, num2, len):
     num1_row = num1//len
     num1_col = num1 % len
@@ -15,43 +14,49 @@ def adjacent(num1, num2, len):
         return False
     return True
 
+# return True is word is in board
+# "used" says which letters have been used so far
+#
 def find_word(board, word, used, n, edge):
     nused = len(used)
     if nused:
         last = used[nused-1]
     for i in range(n):
         if nused and i == last:
-            continue;
+            continue
         if nused and not adjacent(i, last, edge):
-            continue;
+            continue
         if board[i] != word[0]:
             continue
         if nused and i in used:
             continue
         used.append(i)
         if len(word) == 1:
-            #print(used)
             return True
         return find_word(board, word[1:], used, n, edge)
     return False
-    
- 
-def solve(board):
+
+min_length = 4
+
+# print list of all words in board
+#
+def solve(board, jumbo):
     n = len(board)
-    edge = math.sqrt(n)
+    edge = int(math.sqrt(n))
     count = 0
-    for w in get_dictionary(False, True):
-        if len(w) < 4:
-            continue;
-        if find_word(b, w, [], n, edge):
+    for w in get_dictionary(jumbo, True):
+        if len(w) < min_length:
+            continue
+        if find_word(board, w, [], n, edge):
             print(w)
             count += 1
-    print('count: ', count)
-    
+    print 'count: ', count
+
 b = ['a', 'n', 'e', 'r',
      's', 'e', 'h', 't',
      'm', 'o', 'l', 'i',
      'w', 'b', 'c', 'v']
 
-solve(b)
-    
+x = list(sys.argv[1])
+min_length = int(sys.argv[2])
+solve(x, len(sys.argv)>3)
